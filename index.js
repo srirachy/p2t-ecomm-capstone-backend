64b767bd91087857f451f00dbc82fe6b3bd36a27
srirachy <caseyereyes@gmail.com>
2025-04-29 07:21:31 -0700

    add starting code for mongodb connection

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected.');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    }
+}
+
+app.get('/', (_req, res) => {
+    res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+const DEPLOYED_URI = process.env.DEPLOYED_URI;
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on ${DEPLOYED_URI ? DEPLOYED_URI : `http://localhost:${PORT}`}`);
+})
