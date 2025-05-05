e09b197bb79a89ea0b3e050cb5e32385aaf48471
srirachy <caseyereyes@gmail.com>
2025-05-05 09:12:06 -0700

    add initial code for user routes

--- index.js
+++ index.js
@@ -28,9 +28,12 @@ app.get('/', (_req, res) => {
     res.send('API is running');
 });
 
+app.use('/users', userRoutes);
+app.use('/admin/users', userRoutes);
+
 const PORT = process.env.PORT || 5000;
 const DEPLOYED_URI = process.env.DEPLOYED_URI;
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running on ${DEPLOYED_URI ? DEPLOYED_URI : `http://localhost:${PORT}`}`);
-})
+});
