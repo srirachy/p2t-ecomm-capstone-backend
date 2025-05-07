8206c8abc7ac4e3fb2bee6f612470b7d049dc32b
srirachy <caseyereyes@gmail.com>
2025-05-06 18:49:13 -0700

    add auth0 and jwt along with private route examples

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import { auth, requiredScopes } from 'express-oauth2-jwt-bearer'
 // import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 
 const app = express();
 
+const checkJwt = auth({
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_BASE_URL,
+    tokenSigningAlg: 'RS256'
+});
 const corspolicy = {
     'origin': process.env.FRONTEND_URI,
 } //allow origin
@@ -17,6 +23,22 @@ app.use(express.json());
 
 app.get('/', (_req, res) => {
     res.send('API is running');
+});
+
+app.get('/api/private', checkJwt, (_req, res) => {
+    res.json({
+        message: 'hello from private endpoint! you need to be authenticated to see this'
+    })
+});
+
+const checkScopes = requiredScopes('read:products');
+
+app.get('/api/private-scoped', checkJwt, checkScopes, (_req, res) => {
+    res.json('hello from private endpoint! you need auth and scope to read this!');
+});
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
 });
 
 // app.use('/users', userRoutes);
